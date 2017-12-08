ae4410dd0b9c6d7ed7c05cf391a34b699fcd0562
YagishitaAkihiro <akari_manygods_629@yahoo.co.jp>
2017-12-08 15:12:42 +0900

    regenerate

---
+++ scripts/old_prog/vive_telexistence_master.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author: Akihiro Yagishita
+
+from nextage_ros_bridge import nextage_client
+from hrpsys import rtm
+import argparse
+from hironx_ros_bridge.ros_client import ROS_Client
+
+# depends
+import rospy
+import math
+import tf
+from geometry_msgs.msg import Vector3
+
+class Tele():
+      def quaternion_to_euler(self,rot):
+             self.e = tf.transformations.euler_from_quaternion((rot[0], rot[1], rot[2], rot[3]))
+             return Vector3(x=self.e[0], y=self.e[1], z=self.e[2])
+
+      def __init__(self):
+         '''
+         @param nxo_if: type nextage_client.NextageClient         
+         _robot = nxo_if 今回"ros = ROS_Client()"で代用できた。
+         '''
+#-------------------初期位置を取得-------------------------------------------------------------------
+         listener = tf.TransformListener()
+         rospy.sleep(1)
+         #rospy.Time(0)指定して最新のtransformを取得。
+         now = rospy.Time(0)
+         try:
+             rospy.loginfo("waiting hands and head tf")
+             listener.waitForTransform("/target", "/l_hand", now, rospy.Duration(1.0))
+             listener.waitForTransform("/target", "/r_hand", now, rospy.Duration(1.0))
+             listener.waitForTransform("/target", "/hmd_head", now, rospy.Duration(1.0))
+             (l_trans,l_rot) = listener.lookupTransform('/target', '/l_hand', now)
+             (r_trans,r_rot) = listener.lookupTransform('/target', '/r_hand', now)
+             (h_trans,h_rot) = listener.lookupTransform("/target", "/hmd_head", now)
+             rospy.loginfo("complete")
+         except  (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                  print "tf error"
+
+         #最初の/target /... の距離
+         lkX = round(l_trans[2],2)
+         lkY = round(l_trans[0],2)
+         lkZ = round(l_trans[1],2)
+
+         rkX = round(r_trans[2],2)
+         rkY = round(r_trans[0],2)
+         rkZ = round(r_trans[1],2)
+
+         #hiroのinitial時のposition(そのうちlistにしなおす)
+         lHX = 0.3255
+         lHY = 0.1823
+         lHZ = 0.0746
+
+         lx = -0.6
+         ly = 0
+         lz = -100
+
+         rHX = 0.3255
+         rHY = -0.1823
+         rHZ = 0.0746
+
+         rx = 0.6
+         ry = 0
+         rz = -100
+
+#------------------------
+         self.quaternion_to_euler(l_rot)
+         ldx = round(self.e[2],1)
+         ldy = round(self.e[1],1)
+         ldz = round(self.e[0],1)
+
+         self.quaternion_to_euler(r_rot)
+         rdx = round(self.e[2],1)
+         rdy = round(self.e[1],1)
+         rdz = round(self.e[0],1)
+
+         roll_x = 0.0
+         pitch_y = -1.6
+         yaw_z = -0.05
+         #0, -1.6, -0.05
+
+         self.quaternion_to_euler(h_rot)
+         c_head0 = -round(self.e[1],2)
+         c_head1 = -round(self.e[0],2)
+
+         b_head0 = 0.0
+         b_head1 = 0.0 #上下
+#------------------------
+         while not rospy.is_shutdown():
+               try:
+                  #torsoから見たleft_hand_1の座標。ros::Time(0)指定して最新のl_transformを取得。
+#                  now = rospy.Time.now()
+                  now = rospy.Time(0)
+                  listener.waitForTransform("/target", "/l_hand", now, rospy.Duration(10.0))
+                  listener.waitForTransform("/target", "/r_hand", now, rospy.Duration(10.0))
+                  listener.waitForTransform("/target", "/hmd_head", now, rospy.Duration(10.0))
+                  (l_trans,l_rot) = listener.lookupTransform("/target", "/l_hand", now)
+                  (r_trans,r_rot) = listener.lookupTransform("/target", "/r_hand", now)
+                  (h_trans,h_rot) = listener.lookupTransform("/target", "/hmd_head", now)
+               except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                  continue
+
+#--------------generate_move_plan------------------------------
+               l_transx = lHX - (round(l_trans[2],2) + lkX)
+               l_transy = lHY - (round(l_trans[0],2) - lkY)
+               l_transz = lHZ + (round(l_trans[1],2) - lkZ)
+
+               self.quaternion_to_euler(l_rot)
+               lrotx = roll_x  + round(self.e[0],1) - ldx
+               lroty = pitch_y -(round(self.e[1],1) - ldy)
+               lrotz = yaw_z   + round(self.e[2],1) - ldz
+
+               r_transx = rHX - (round(r_trans[2],2) + rkX)
+               r_transy = rHY - (round(r_trans[0],2) - rkY)
+               r_transz = rHZ + (round(r_trans[1],2) - rkZ)
+
+               self.quaternion_to_euler(r_rot)
+               rrotx = roll_x  + round(self.e[0],1) - rdx
+               rroty = pitch_y -(round(self.e[1],1) - rdy)
+               rrotz = yaw_z   + round(self.e[2],1) - rdz
+
+
+               self.quaternion_to_euler(h_rot)
+               head0 = -round(self.e[1],2) - c_head0
+               head1 = -round(self.e[0],2) - c_head1
+#-------------------------------------------------------------
+#filter  
+
+               if -0.292 > l_transx:
+                  l_transx=-0.292
+               if l_transx > 0.523:
+                  l_transx=0.523
+
+               if -0.150 > l_transy:
+                  l_transy=-0.150
+               if l_transy > 0.6:#0.501:
+                  l_transy=0.6#0.501
+
+               if -0.0 > l_transz:
+                  l_transz= -0.0
+               if l_transz > 0.600:
+                  l_transz=0.600
+#----------------------------------------
+               if -0.292 > r_transx:
+                  r_transx=-0.292
+               if r_transx > 0.523:
+                  r_transx=0.523
+
+               if 0.150 < r_transy:
+                  r_transy=0.150
+               if r_transy < -0.6:#0.501:
+                  r_transy=-0.6#0.501
+
+               if -0.0 > l_transz:
+                  r_transz=-0.0
+               if r_transz > 0.600:
+                  r_transz=0.600
+
+
+#--------------head_filter--------
+               if head0 > 0.7:
+                  head0 = 0.7
+               if head0 < -0.7:
+                  head0 = -0.7
+
+               if head1 > 0.3:
+                  head1 = 0.3
+               if head1 < -0.3:
+                  head1 = -0.3
+
+#--------------move_hand----------
+ #              robot.setTargetPose("larm",[round((l_transx+lx)/2,2),round((l_transy+ly)/2,2),round((l_transz+lz)/2,2)], [0.0, -1.6, -0.05],0.5)
+ #              robot.setTargetPose("rarm",[round((r_transx+rx)/2,2),round((r_transy+ry)/2,2),round((r_transz+rz)/2,2)], [0.0, -1.6, -0.05],0.5)
+
+#--------------filter_update------
+               lx = l_transx
+               ly = l_transy
+               lz = l_transz
+
+               rx = r_transx
+               ry = r_transy
+               rz = r_transz
+#-------------move_head-------------------------------
+               ros.set_joint_angles_rad("head",[-round((b_head0 + head0)/2,3),round((b_head1 + head1)/2,3)],duration=0.2,wait=True) 
+#               ros.set_joint_angles_rad("head",[-round((b_head0 + head0)/2,3),0],duration=0.4,wait=False)   
+               b_head0 = head0
+               b_head1 = head1
+               
+#--------------wait_data-------
+               rospy.sleep(0.5)
+
+if __name__ == '__main__':
+
+#-------------------------------------------initial_setting------------------------------------------
+    parser = argparse.ArgumentParser(description='NEXTAGE Open command line interpreters')
+    parser.add_argument('--host', help='corba name server hostname')
+    parser.add_argument('--port', help='corba name server port number')
+    parser.add_argument('--modelfile', help='robot model file nmae')
+    parser.add_argument('--robot', help='robot modlule name (RobotHardware0 for real robot, Robot()')
+    args, unknown = parser.parse_known_args()
+  
+    if args.host:
+       rtm.nshost = args.host
+    if args.port:
+       rtm.nsport = args.port
+    if not args.robot:
+       args.robot = "RobotHardware0" if args.host else "HiroNX(Robot)0"
+    if not args.modelfile:
+       args.modelfile = ""
+  
+    if len(unknown) >= 2:
+       args.robot = unknown[0]
+       args.modelfile = unknown[1]
+    robot = nxc = nextage_client.NextageClient()
+    robot.init(robotname=args.robot, url=args.modelfile)
+#   24行目の呼び出しの代用品
+    ros = ROS_Client()
+#--------------------------------------------end_initial_setting------------------------------------------
+#   主処理
+    Tele()
---
+++ scripts/re_tele.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from nextage_ros_bridge import nextage_client
+from hrpsys import rtm
+from hironx_ros_bridge.ros_client import ROS_Client
+import argparse
+import rospy
+import math
+import tf
+from geometry_msgs.msg import Vector3
+
+#(left_x,left_y,left_z,right_x,right_y,right_z)
+ini_p = (0.3255, 0.1823, 0.0746, 0.3255, -0.1823, 0.0746)
+ini_f = [-0.6,   0.0,   -100.0,  0.6,     0.0,   -100.0]
+ini_ang = [0.0, -1.6, -0.05] #タプルにせねば。
+
+low_filter = [-0.6, 0.0, -100.0, 0.6, 0.0, -100.0]#ローパスフィルター用初期値
+
+class Tele():
+      def q2e(self,rot):
+          self.eul = tf.transformations.euler_from_quaternion((rot[0],rot[1],rot[2],rot[3]))
+          return Vector(x=self.eul[0],y=self.eul[1],z=self.eul[2])
+
+      def __init__(self):
+          robot.goInitial()
+          listener =tf.TransformListener()
+          listener2=tf.TransformListener()
+          rospy.sleep(1)
+          now = rospy.Time(0)
+          rospy.loginfo("waiting hands and head tf")
+          try:
+              listener.waitForTransform("/target", "/l_hand", now, rospy.Duration(1.0))
+              listener.waitForTransform("/target", "/r_hand", now, rospy.Duration(1.0))
+              listener.waitForTransform("/target", "/hmd_head", now, rospy.Duration(1.0))
+              (l_trans,l_rot) = listener.lookupTransform('/target', '/l_hand', now)
+              (r_trans,r_rot) = listener.lookupTransform('/target', '/r_hand', now)
+              (h_trans,h_rot) = listener.lookupTransform("/target", "/hmd_head", now)
+          except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+              rospy.loginfo("TF not broadcast")
+          rospy.loginfo("complete")
+          
+          initial_left = (round(l_trans[2],2),round(l_trans[0],2),round(l_trans[1],2))
+          initial_right= (round(r_trans[2],2),round(r_trans[0],2),round(r_trans[1],2))
+          while not rospy.is_shutdown():
+                now = rospy.Time(0)
+                try:
+                    (l_trans2,l_rot2) = listener2.lookupTransform('/target', '/l_hand', now)
+                    (r_trans2,r_rot2) = listener2.lookupTransform('/target', '/r_hand', now)
+                    (h_trans2,h_rot2) = listener2.lookupTransform("/target", "/hmd_head", now) 
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    continue
+
+#---------------Vector------------------------------------------------------------------
+                global ini_p
+
+                L_dis = [-(l_trans2[2] - initial_left[0]), #どれだけうごいたか 
+                         -(l_trans2[0] - initial_left[1]),
+                          (l_trans2[1] - initial_left[2])]
+
+                R_dis = [-(r_trans2[2] - initial_right[0]), #どれだけうごいたか 
+                         -(r_trans2[0] - initial_right[1]),
+                          (r_trans2[1] - initial_right[2])]
+
+#---------------------------------------------------------------------------------
+                global low_filter
+
+                LTP = [round((ini_p[0]+L_dis[0]+low_filter[0])/2,2),
+                       round((ini_p[1]+L_dis[1]+low_filter[1])/2,2),
+                       round((ini_p[2]+L_dis[2]+low_filter[2])/2,2)]
+
+                RTP = [round((ini_p[3]+R_dis[0]+low_filter[3])/2,2),
+                       round((ini_p[4]+R_dis[1]+low_filter[4])/2,2),
+                       round((ini_p[5]+R_dis[2]+low_filter[5])/2,2)]
+ 
+#----------------------------------------------------
+                if -0.292 > LTP[0]:
+                  LTP[0] =-0.292
+                if LTP[0] > 0.523:
+                  LTP[0] =0.523
+
+                if -0.150 > LTP[1]:
+                  LTP[1] = -0.150
+                if LTP[1] > 0.6:#0.501:
+                  LTP[1] = 0.6 #0.501
+
+                if -0.0 > LTP[2]:
+                  LTP[2] = -0.0
+                if LTP[2] > 0.600:
+                  LTP[2] =0.600
+
+                if -0.292 > RTP[0]:
+                   RTP[0] = 0.292
+                if RTP[0] > 0.523:
+                   RTP[0] = 0.523
+
+                if 0.150 < RTP[1]:
+                   RTP[1] = 0.150
+                if RTP[1] < -0.6:
+                   RTP[1] = -0.6
+
+                  
+#---------------------------------------------------------------------------------
+                global ini_ang
+                robot.setTargetPose("larm",LTP, ini_ang,0.3)
+                robot.setTargetPose("rarm",RTP, ini_ang,0.3)
+                #ros.set_pose("rarm",RTP,ini_ang,0.2)
+#                ros.set_pose("larm",LTP,ini_ang,1.0)
+                rospy.sleep(0.3)
+
+                #ローパスフィルター_アップデート
+                r_cur_p = robot.getCurrentPosition("RARM_JOINT5")
+                l_cur_p = robot.getCurrentPosition("LARM_JOINT5")
+                low_filter = [l_cur_p[0], l_cur_p[1], l_cur_p[2], r_cur_p[0], r_cur_p[1], r_cur_p[2]]
+
+if __name__ == '__main__':
+
+#-------------------------------------------initial_setting------------------------------------------
+    parser = argparse.ArgumentParser(description='NEXTAGE Open command line interpreters')
+    parser.add_argument('--host', help='corba name server hostname')
+    parser.add_argument('--port', help='corba name server port number')
+    parser.add_argument('--modelfile', help='robot model file nmae')
+    parser.add_argument('--robot', help='robot modlule name (RobotHardware0 for real robot, Robot()')
+    args, unknown = parser.parse_known_args()
+  
+    if args.host:
+       rtm.nshost = args.host
+    if args.port:
+       rtm.nsport = args.port
+    if not args.robot:
+       args.robot = "RobotHardware0" if args.host else "HiroNX(Robot)0"
+    if not args.modelfile:
+       args.modelfile = ""
+  
+    if len(unknown) >= 2:
+       args.robot = unknown[0]
+       args.modelfile = unknown[1]
+    robot = nxc = nextage_client.NextageClient()
+    robot.init(robotname=args.robot, url=args.modelfile)
+    ros = ROS_Client()
+#--------------------------------------------end_initial_setting------------------------------------------
+#   主処理
+    Tele()
